f82c09d9a13775a13f34e5e51febc5a4d7d53213
NahiyaNasir <nahiaesha.01@gmail.com>
2025-11-26 18:02:55 +0600

    encapsulation

---
+++ src/encapsulation.ts
@@ -0,0 +1,30 @@
+
+class BankAccount{
+   public readonly userId: number;
+  public userName: string;
+  private _userBalance: number;
+
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this._userBalance = userBalance;
+  }
+
+  private addBalance(balance: number) {
+    this._userBalance = this._userBalance + balance;
+  }
+ 
+  callHiddenMethod(balance:number){
+    this.addBalance(balance)
+  }
+
+}
+
+class StudentBankAccount extends BankAccount {
+  test() {
+   
+  }
+}
+  const sty= new StudentBankAccount(111, "Mezba",100)
+  sty.callHiddenMethod(988)
+  console.log(sty);
